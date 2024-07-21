3cca4808190c084cc1678de7eae1dc09af551b68
Ian Speelman <ianspeelman@gmail.com>
2024-07-21 19:43:19 +0200

    testing

--- network/tests.py
+++ network/tests.py
@@ -1,14 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from . import models
+import time
 
 User = models.User
+c = Client()
 
 # Create your tests here.
 class TestRegister(TestCase):
     def setUp(self):
-        user = User.objects.create_user("spielmeister", "ianspeelman@gmail.com", "wajogino")
-        user.save()
+        c.post("/register", {"username": "meister", "email": "ianspeelman@gmail.com", "password":"wajogino", "confirmation": "wajogino"})
     
     def test_user_existence(self):
-        ian = User.objects.get(email="ianspeelman@gmail.com")
-        self.assertEqual(ian.email, "ianspeelman@gmail.com")
+        user = User.objects.get(username="meister")
+        self.assertEqual(user.username, "meister")
